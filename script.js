e318b1833da90a1ef542263ba86f9e7b38a4f4f0
manichino <jcalabrese15@gmail.com>
2024-05-20 21:39:45 -0700

    added final javascript and html

--- script.js
+++ script.js
@@ -1,23 +1,64 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+$(document).ready(function(){
+function updateTime() {
+  let today = dayjs();
+
+  // updates the time element in the header
+  $("#currentDay").text(today.format("dddd, MMMM D YYYY, h:mm:ss A"));
+
+  // For coloring the past, present, and future time blocks
+  let now = parseInt(dayjs().format("H"));
+  $(".time-block").each(function() {
+    let blockHour = parseInt($(this).attr("data-hour"));
+    $(this).removeClass("future past present");
+
+    if (now > blockHour) {
+        $(this).addClass("past");
+    } else if (now === blockHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+  });
+}
+
+// textarea elements
+let saveBtn = $(".saveBtn");
+let scheduleElArray = $(".time-block");
+
+renderLastRegistered();
+updateTime();
+setInterval(updateTime, 1000); 
+
+// render schedule saved in local storage
+function renderLastRegistered() {
+  scheduleElArray.each(function() {
+    let key = "time block " + $(this).attr("data-hour");
+    $(this).find("textarea").val(localStorage.getItem(key));
+  });
+}
+
+// function for handling clicks
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  let btnClicked = $(event.currentTarget);
+  let targetText = btnClicked.siblings("textarea");
+  let targetTimeBlock = btnClicked.closest(".time-block").data("hour");
+
+  // Debugging: Log textarea value and targetTimeBlock
+  console.log("Textarea Value:", targetText.val());
+  console.log("Data Hour:", targetTimeBlock);
+
+  // Save data to local storage
+  localStorage.setItem("time block " + targetTimeBlock, targetText.val());
+}
+
+saveBtn.on("click", handleFormSubmit); 
+
+renderLastRegistered();
+updateTime();
+setInterval(updateTime, 1000);
+})
